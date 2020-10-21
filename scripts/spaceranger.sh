6ee89301ce004b08faae6f8f8527b06c06f08af1
Lukas Weber <lukas.weber.edu@gmail.com>
2020-10-21 05:53:47 -0400

    example script to run Space Ranger

---
+++ scripts/spaceranger.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+############################
+# Script to run Space Ranger
+############################
+
+# example code to run Space Ranger
+# adapted from Andrew Jaffe's script 'pilot_align.sh'
+
+# run on JHPCE cluster
+# qrsh -pe local 8 -l mem_free=3G,h_vmem=6G,h_fsize=100G -now n
+
+# using local installation of spaceranger in home directory
+
+
+# run spaceranger count
+spaceranger count --id=LC_1 \
+--transcriptome=../spaceranger/refdata-gex-GRCh38-2020-A \
+--fastqs=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/FASTQ/LC_1 \
+--image=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/Snapshots_Plus_Loupe/Lieber-Institute_OTS-20-7043_Pt1.jpg \
+--slide=V19B23-076 --area=B1 \
+--loupe-alignment=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/Snapshots_Plus_Loupe/V19B23-076-B1.json \
+--localcores=8 \
+--localmem=20
+
