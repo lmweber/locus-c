ae2b453ea30cbf0d5d4fb823a847a0ef938c0cbb
Stephanie Hicks <stephaniechicks@gmail.com>
2020-10-27 15:38:05 -0400

    updating spaceranger.sh file with manually aligned json image files

--- scripts/spaceranger.sh
+++ scripts/spaceranger.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#$ -pe local 8
+#$ -l mem_free=10G,h_vmem=12G,h_fsize=100G
+#$ -V
+#$ -cwd
 
 ############################
 # Script to run Space Ranger
@@ -17,7 +21,6 @@
 # summary spreadsheet: /dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Visium LC pilot_072120 Master.xlsx
 # - contains sample ID, sample name, slide serial number, capture area ID
 
-
 # run on JHPCE cluster
 # qsub -V -cwd -pe local 8 -l mem_free=30G,h_vmem=40G,h_fsize=300G scripts/spaceranger.sh
 
@@ -31,16 +34,48 @@ cd ../outputs
 
 # run spaceranger count
 spaceranger count \
+--id=DLPFC \
+--transcriptome=/dcl02/leased/shicks/spaceranger/refdata-gex-GRCh38-2020-A \
+--fastqs=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/FASTQ/DLPFC \
+--image=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/Raw/Lieber-Institute_OTS-20-7043_1_1.tif \
+--slide=V19B23-076 \
+--area=A1 \
+--loupe-alignment=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/json_after_manual_loupe/V19B23-076-A1.json \
+--localcores=8 \
+--localmem=64
+
+spaceranger count \
 --id=LC_1 \
 --transcriptome=/dcl02/leased/shicks/spaceranger/refdata-gex-GRCh38-2020-A \
 --fastqs=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/FASTQ/LC_1 \
 --image=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/Raw/Lieber-Institute_OTS-20-7043_1_2.tif \
 --slide=V19B23-076 \
 --area=B1 \
---nosecondary \
+--loupe-alignment=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/json_after_manual_loupe/V19B23-076-B1.json \
 --localcores=8 \
---localmem=200 \
---localvmem=400
+--localmem=64
+
+spaceranger count \
+--id=LC_2 \
+--transcriptome=/dcl02/leased/shicks/spaceranger/refdata-gex-GRCh38-2020-A \
+--fastqs=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/FASTQ/LC_2 \
+--image=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/Raw/Lieber-Institute_OTS-20-7043_1_3.tif \
+--slide=V19B23-076 \
+--area=C1 \
+--loupe-alignment=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/json_after_manual_loupe/V19B23-076-C1.json \
+--localcores=8 \
+--localmem=64
+
+spaceranger count \
+--id=LC_3 \
+--transcriptome=/dcl02/leased/shicks/spaceranger/refdata-gex-GRCh38-2020-A \
+--fastqs=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/FASTQ/LC_3 \
+--image=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/Raw/Lieber-Institute_OTS-20-7043_1_4.tif \
+--slide=V19B23-076 \
+--area=D1 \
+--loupe-alignment=/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/MiSeq_Pilot/Images/json_after_manual_loupe/V19B23-076-D1.json \
+--localcores=8 \
+--localmem=64
 
 # restore working directory
 cd $cwd
