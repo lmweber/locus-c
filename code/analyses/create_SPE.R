467ca62f6d49fb32b8ffea697ff739b266025b0f
Lukas Weber <lukas.weber.edu@gmail.com>
2021-09-14 16:06:12 -0400

    script to create combined SPE - metadata

---
+++ code/analyses/create_SPE.R
@@ -0,0 +1,49 @@
+###########################################
+# LC project
+# Script to create SpatialExperiment object
+# Lukas Weber, Sep 2021
+###########################################
+
+# Create a single SpatialExperiment object containing all samples
+
+
+library(SpatialExperiment)
+library(here)
+
+
+# ---------------
+# set up metadata
+# ---------------
+
+n_round1 <- 2
+n_round2 <- 3
+n_round3 <- 4
+
+sample_ids <- c(
+  "Br6522_LC_1_round1", "Br6522_LC_2_round1", 
+  "Br8153_LC_round2", "Br5459_LC_round2", "Br2701_LC_round2", 
+  "Br6522_LC_round3", "Br8079_LC_round3", "Br2701_LC_round3", "Br8153_LC_round3"
+)
+
+rounds <- c(
+  rep("round1", n_round1), 
+  rep("round2", n_round2), 
+  rep("round3", n_round3)
+)
+
+paths <- here("processed_data", "spaceranger", 
+              c(rep("NextSeqMiSeq", n_round1), 
+                rep("Linda_2021-05-21", n_round2), 
+                rep("KMay_2021-07-09", n_round3)))
+
+stopifnot(length(sample_ids) == length(rounds))
+stopifnot(length(sample_ids) == length(paths))
+
+df_samples <- data.frame(
+  sample_id = sample_ids, 
+  round_id = rounds, 
+  path = paths
+)
+
+df_samples
+
