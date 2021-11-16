4e6f95e1904d563f3cadbcd2f03fcf6125179b20
Lukas Weber <lukas.weber.edu@gmail.com>
2021-11-15 20:04:48 -0500

    code to run and deploy Shiny app

---
+++ code/analyses/5_shiny_app/app.R
@@ -0,0 +1,60 @@
+## Code to run and deploy Shiny app
+## Code provided by Leonardo Collado-Torres
+## Available at:
+## https://github.com/LieberInstitute/Visium_IF_AD/tree/master/code/05_deploy_app
+
+
+library(spatialLIBD)
+library(markdown)
+library(here)
+
+
+## spatialLIBD uses golem
+options("golem.app.prod" = TRUE)
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Load the data
+spe <- readRDS(here("processed_data", "SPE", "LCrounds1to3_SPE_shiny.rds"))
+
+## Create a soft link to the data, otherwise rsconnect::deployApp doesn't work
+## Delete if already exists
+system("rm LCrounds1to3_SPE_shiny.rds")
+system("ln -s processed-data/SPE/LCrounds1to3_SPE_shiny.rds LCrounds1to3_SPE_shiny.rds")
+
+vars <- colnames(colData(spe))
+
+## Deploy the website
+spatialLIBD::run_app(
+  spe, 
+  sce_layer = NULL, 
+  modeling_results = NULL, 
+  sig_genes = NULL, 
+  title = "Locus coeruleus", 
+  spe_discrete_vars = c(
+    vars[grep("10x_", vars)], 
+    "ManualAnnotation"
+  ), 
+  spe_continuous_vars = c(
+    "sum_umi", 
+    "sum_gene", 
+    "expr_chrM", 
+    "expr_chrM_ratio"
+    #"NAbeta", 
+    #"PAbeta", 
+    #"NDAPI", 
+    #"PDAPI", 
+    #"NGFAP", 
+    #"PGFAP", 
+    #"NLipofuscin", 
+    #"PLipofuscin", 
+    #"NMAP2", 
+    #"PMAP2", 
+    #"NpTau", 
+    #"PpTau"
+  ),
+  default_cluster = "ManualAnnotation"
+  #default_cluster = "10x_graphclust"
+)
+
---
+++ code/analyses/5_shiny_app/deploy.R
@@ -0,0 +1,34 @@
+## Code to run and deploy Shiny app
+## Code provided by Leonardo Collado-Torres
+## Available at:
+## https://github.com/LieberInstitute/Visium_IF_AD/tree/master/code/05_deploy_app
+
+
+library(rsconnect)
+library(here)
+
+
+## Or you can go to your shinyapps.io account and copy this
+## Here we do this to keep our information hidden
+load(here("code", "analyses", "5_shiny_app", ".deploy_info.Rdata"), verbose = TRUE)
+rsconnect::setAccountInfo(
+  name = deploy_info$name, 
+  token = deploy_info$token, 
+  secret = deploy_info$secret
+)
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Deploy the app, that is, upload it to shinyapps.io
+rsconnect::deployApp(
+  appDir = here("code", "analyses", "5_shiny_app", "05_deploy_app"), 
+  appFiles = c(
+    "app.R", 
+    "spe.Rdata"
+  ), 
+  appName = 'Locus_coeruleus', 
+  account = 'lmweber', 
+  server = 'shinyapps.io'
+)
+
