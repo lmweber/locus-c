fb8b62b4b3e0966c833748f44d72757d1ba63ecc
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-22 15:17:16 -0400

    additional QC separately in LC and WM regions

--- code/analyses/02_quality_control/LC_quality_control.R
+++ code/analyses/02_quality_control/LC_quality_control.R
@@ -16,6 +16,7 @@ library(here)
 library(scater)
 library(harmony)
 library(ggplot2)
+library(ggnewscale)
 
 
 # directory to save plots
@@ -68,11 +69,11 @@ dim(spe_LC)
 dim(spe_WM)
 
 
-# ------------------------
-# spot-level QC: all spots
-# ------------------------
+# -----------------------------------------------------
+# spot-level QC: all spots (LC and WM regions together)
+# -----------------------------------------------------
 
-# note: for checking only; perform separate QC in LC vs. WM regions below
+# not used for final QC
 
 # identify mitochondrial genes
 is_mito <- grepl("(^MT-)|(^mt-)", rowData(spe)$gene_name)
@@ -155,38 +156,26 @@ thresh_low_n_features ## 25.2
 thresh_high_subsets_mito_percent <- attr(reasons$high_subsets_mito_percent, "thresholds")["higher"]
 thresh_high_subsets_mito_percent  ## 63.4
 
-# store in SPE object
+# store in subsetted SPE object
 stopifnot(nrow(reasons) == nrow(colData(spe_LC)))
 colData(spe_LC) <- cbind(colData(spe_LC), reasons)
 
+# store in main SPE object
+colData(spe)$discard <- FALSE
+colData(spe)[colData(spe_LC)$key_id, "discard"] <- colData(spe_LC)$discard
 
-# plot discarded spots
-
-table(colData(spe_LC)$sample_part_id, colData(spe_LC)$discard)
-
-df <- cbind.data.frame(colData(spe_LC), spatialCoords(spe_LC))
-
-ggplot(df, aes(x = pxl_col_in_fullres, y = pxl_row_in_fullres, color = discard)) + 
-  facet_wrap(~ sample_id, nrow = 2) + 
-  geom_point(size = 0.1) + 
-  coord_fixed() + 
-  scale_y_reverse() + 
-  scale_color_manual(values = c("gray85", "red")) + 
-  ggtitle("Spot-level QC") + 
-  theme_bw() + 
-  theme(panel.grid = element_blank(), 
-        axis.title = element_blank(), 
-        axis.text = element_blank(), 
-        axis.ticks = element_blank())
-
-fn <- file.path(dir_plots, "01_quality_control", "QC_discard_LCregions")
-ggsave(paste0(fn, ".pdf"), width = 12, height = 6.5)
-ggsave(paste0(fn, ".pnf"), width = 12, height = 6.5)
+# check
+table(colData(spe_LC)$discard)
+table(colData(spe)$discard)
 
 
 # -------------------------
 # spot-level QC: WM regions
 # -------------------------
+
+# check number of spots
+dim(spe_WM)
+table(colData(spe_WM)$sample_id)
 
 # identify mitochondrial genes
 is_mito <- grepl("(^MT-)|(^mt-)", rowData(spe_WM)$gene_name)
@@ -196,8 +185,15 @@ rowData(spe_WM)$gene_name[is_mito]
 # calculate QC metrics using scater package
 spe_WM <- addPerCellQC(spe_WM, subsets = list(mito = is_mito))
 
+
+# check summaries and compare with combined object
 summary(colData(spe_WM)$sum)
+summary(colData(spe_LC)$sum)
+summary(colData(spe)$sum)
+
 summary(colData(spe_WM)$detected)
+summary(colData(spe_LC)$detected)
+summary(colData(spe)$detected)
 
 
 # plot histograms of QC metrics
@@ -210,6 +206,78 @@ hist(colData(spe_WM)$subsets_mito_percent, xlab = "percent mito", main = "Percen
 hist(colData(spe_WM)$cell_count, xlab = "no. cells", main = "No. cells per spot")
 par(mfrow = c(1, 1))
 dev.off()
+
+
+# select adaptive thresholds using 3 MAD methodology from OSCA
+reasons <- perCellQCFilters(perCellQCMetrics(spe_WM, subsets = list(mito = is_mito)), 
+                            sub.fields = "subsets_mito_percent")
+colSums(as.matrix(reasons))
+
+# check thresholds
+thresh_low_lib_size <- attr(reasons$low_lib_size, "thresholds")["lower"]
+thresh_low_lib_size  ## 1.3
+thresh_low_n_features <- attr(reasons$low_n_features, "thresholds")["lower"]
+thresh_low_n_features ## 1.4
+thresh_high_subsets_mito_percent <- attr(reasons$high_subsets_mito_percent, "thresholds")["higher"]
+thresh_high_subsets_mito_percent  ## 69.6
+
+# store in subsetted SPE object
+stopifnot(nrow(reasons) == nrow(colData(spe_WM)))
+colData(spe_WM) <- cbind(colData(spe_WM), reasons)
+
+# store in main SPE object
+colData(spe)[colData(spe_WM)$key_id, "discard"] <- colData(spe_WM)$discard
+
+# check
+table(colData(spe_WM)$discard)
+table(colData(spe_LC)$discard)
+table(colData(spe)$discard)
+
+
+# --------------------
+# plot discarded spots
+# --------------------
+
+# summary of discarded spots in LC and WM regions
+table(colData(spe_LC)$sample_part_id, colData(spe_LC)$discard)
+table(colData(spe_WM)$sample_id, colData(spe_WM)$discard)
+
+# plot all spots
+df <- cbind.data.frame(colData(spe), spatialCoords(spe))
+
+ggplot(df, aes(x = pxl_col_in_fullres, y = pxl_row_in_fullres)) + 
+  facet_wrap(~ sample_id, nrow = 3, scales = "free") + 
+  geom_point(aes(color = in_tissue), size = 0.1) + 
+  scale_color_manual(values = "gray85") + 
+  new_scale_color() + 
+  geom_point(data = df[df$annot_region, , drop = FALSE], 
+             aes(color = annot_region), size = 0.1) + 
+  scale_color_manual(values = "gray70") + 
+  new_scale_color() + 
+  geom_point(data = df[df$discard, , drop = FALSE], 
+             aes(color = discard), size = 0.1) + 
+  scale_color_manual(values = "red") + 
+  scale_y_reverse() + 
+  ggtitle("Spot-level QC") + 
+  theme_bw() + 
+  theme(aspect.ratio = 1, 
+        panel.grid = element_blank(), 
+        axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank())
+
+fn <- file.path(dir_plots, "01_quality_control", "QC_discard_combinedSampleMetrics")
+ggsave(paste0(fn, ".pdf"), width = 7, height = 6.75)
+ggsave(paste0(fn, ".png"), width = 7, height = 6.75)
+
+
+# -----------
+# save object
+# -----------
+
+fn_out <- here("processed_data", "SPE", "LC_preprocessed_QC")
+saveRDS(spe, paste0(fn_out, ".rds"))
+save(spe, file = paste0(fn_out, ".RData"))
 
 
 # --------------
