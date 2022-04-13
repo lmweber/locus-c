06f362662dc5fda544cdd2303153429bb17aa8b5
Lukas Weber <lukas.weber.edu@gmail.com>
2022-04-13 14:30:19 -0400

    downstream plots: clusters and markers

---
+++ code/analyses/05_downstream/LC_downstream_combined.R
@@ -0,0 +1,140 @@
+################################
+# LC project
+# Script for downstream analyses
+# Lukas Weber, Apr 2022
+################################
+
+# module load conda_R/4.1.x
+# Rscript filename.R
+
+# file location:
+# /dcs04/lieber/lcolladotor/pilotLC_LIBD001/locus-c/
+
+
+library(SpatialExperiment)
+library(here)
+library(scater)
+library(scran)
+library(ggplot2)
+
+
+# directory to save plots
+dir_plots <- here("plots", "05_downstream")
+
+
+# ---------
+# load data
+# ---------
+
+# load saved SPE object from previous script
+
+fn_spe <- here("processed_data", "SPE", "LC_batchCorrected.rds")
+spe <- readRDS(fn_spe)
+
+dim(spe)
+
+
+# -------------
+# plot clusters
+# -------------
+
+df <- cbind.data.frame(
+  colData(spe), spatialCoords(spe), 
+  reducedDim(spe, "PCA"), reducedDim(spe, "UMAP"), reducedDim(spe, "HARM")
+)
+
+pal <- c(unname(palette.colors(n = 8, palette = "Okabe-Ito")), 
+         unname(palette.colors(n = 36, palette = "Polychrome 36")))
+
+
+# Harmony embeddings
+ggplot(df, aes(x = HARM1, y = HARM2, color = label)) + 
+  geom_point(size = 0.2, alpha = 0.5) + 
+  scale_color_manual(values = pal) + 
+  ggtitle("Clustering: Harmony embeddings") + 
+  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1))) + 
+  theme_bw() + 
+  theme(panel.grid = element_blank())
+
+fn <- file.path(dir_plots, "LC_clusters_HARM")
+ggsave(paste0(fn, ".pdf"), width = 6.25, height = 5)
+ggsave(paste0(fn, ".png"), width = 6.25, height = 5)
+
+
+# PCA
+ggplot(df, aes(x = PC1, y = PC2, color = label)) + 
+  geom_point(size = 0.2, alpha = 0.5) + 
+  scale_color_manual(values = pal) + 
+  ggtitle("Clustering: PCA") + 
+  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1))) + 
+  theme_bw() + 
+  theme(panel.grid = element_blank())
+
+fn <- file.path(dir_plots, "LC_clusters_PCA")
+ggsave(paste0(fn, ".pdf"), width = 6.25, height = 5)
+ggsave(paste0(fn, ".png"), width = 6.25, height = 5)
+
+
+# UMAP
+ggplot(df, aes(x = UMAP1, y = UMAP2, color = label)) + 
+  geom_point(size = 0.2, alpha = 0.5) + 
+  scale_color_manual(values = pal) + 
+  ggtitle("Clustering: UMAP") + 
+  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1))) + 
+  theme_bw() + 
+  theme(panel.grid = element_blank())
+
+fn <- file.path(dir_plots, "LC_clusters_UMAP")
+ggsave(paste0(fn, ".pdf"), width = 6.25, height = 5)
+ggsave(paste0(fn, ".png"), width = 6.25, height = 5)
+
+
+# x-y space (by sample)
+ggplot(df, aes(x = pxl_col_in_fullres, y = pxl_row_in_fullres, color = label)) + 
+  facet_wrap(~ sample_id, nrow = 3, scales = "free") + 
+  geom_point(size = 0.1) + 
+  scale_color_manual(values = pal) + 
+  # coord_fixed() +  ## use 'aspect.ratio = 1' instead with 'scales = "free"'
+  scale_y_reverse() + 
+  ggtitle("Clustering: x-y space") + 
+  guides(color = guide_legend(override.aes = list(size = 2))) + 
+  theme_bw() + 
+  theme(aspect.ratio = 1, 
+        panel.grid = element_blank(), 
+        axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank())
+
+fn <- file.path(dir_plots, "LC_clusters_XYspace")
+ggsave(paste0(fn, ".pdf"), width = 6.75, height = 7)
+ggsave(paste0(fn, ".png"), width = 6.75, height = 7)
+
+
+# --------------------------
+# marker gene identification
+# --------------------------
+
+# code from OSCA
+
+rownames(spe) <- rowData(spe)$gene_name
+
+marker.info <- scoreMarkers(spe, colLabels(spe))
+marker.info
+
+
+# plot top markers for each cluster
+
+for (i in names(marker.info)) {
+  chosen <- marker.info[[i]]
+  ordered <- chosen[order(chosen$mean.AUC, decreasing = TRUE), ]
+  head(ordered[, 1:4])
+  plotExpression(spe, features = head(rownames(ordered)), 
+                 x = "label", colour_by = "label") + 
+    scale_color_manual(values = pal, name = "label") + 
+    guides(color = guide_legend(override.aes = list(size = 2, alpha = 1)))
+  ggsave(paste0(here(dir_plots, "markers_cluster"), i, ".pdf"), 
+         width = 10, height = 6, bg = "white")
+  ggsave(paste0(here(dir_plots, "markers_cluster"), i, ".png"), 
+         width = 10, height = 6, bg = "white")
+}
+
