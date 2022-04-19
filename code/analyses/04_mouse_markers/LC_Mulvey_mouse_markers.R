66fe765c3720d4fc11894718094161beadb44d33
Lukas Weber <lukas.weber.edu@gmail.com>
2022-04-19 16:54:30 -0400

    update Mulvey mouse markers plots

--- code/analyses/04_mouse_markers/LC_Mulvey_mouse_markers.R
+++ code/analyses/04_mouse_markers/LC_Mulvey_mouse_markers.R
@@ -1,8 +1,8 @@
-#########################################
+###################################################
 # LC project
-# Mulvey et al. (2018) mouse marker genes
-# Lukas Weber, Oct 2021
-#########################################
+# Script to plot Mulvey et al. (2018) mouse markers
+# Lukas Weber, Apr 2022
+###################################################
 
 # module load conda_R/4.1.x
 # Rscript filename.R
@@ -17,21 +17,27 @@ library(biomaRt)
 library(ggplot2)
 
 
+# directory to save plots
+dir_plots <- here("plots", "04_mouse_markers", "Mulvey")
+
+
 # ---------
 # load data
 # ---------
 
-# load SpatialExperiment object
+# load saved SPE object from previous script
 
-fn_spe <- here("processed_data", "SPE", "LCrounds1to3_SPE_processed.rds")
+fn_spe <- here("processed_data", "SPE", "LC_qualityControlled.rds")
 spe <- readRDS(fn_spe)
+
+dim(spe)
 
 
 # ---------------------
 # load marker gene list
 # ---------------------
 
-# list of 45 known LC marker genes for mouse
+# list of 45 known marker genes for LC in mouse
 # from Mulvey et al. (2018), Figure 2A: https://pubmed.ncbi.nlm.nih.gov/29791834/
 
 # load marker gene names saved in text file
@@ -68,6 +74,16 @@ human_markers <- sort(human_markers)
 length(human_markers)
 human_markers
 
+# alternatively:
+# human_markers <- c(
+#   "ASB4", "CALCR", "CHODL", "CYB561", "DBH", "DDC", "DLK1", "EYA2", "FAM183A", 
+#   "FIBCD1", "GAL", "GCH1", "GLRA2", "GNG4", "GPX3", "HCRTR1", "MAOA", "MRAP2", 
+#   "MYOM2", "NXPH4", "OVGP1", "PCBD1", "PHOX2A", "PHOX2B", "SLC18A2", "SLC31A1",
+#   "SLC6A2", "STBD1", "SYT17", "TH", "TM4SF1", "TRAF3IP2")
+# 
+# length(human_markers)
+# length(human_markers %in% rowData(spe)$gene_name)
+
 
 # ---------------
 # plot expression
@@ -75,14 +91,15 @@ human_markers
 
 # plot expression for each marker in each sample
 
-sample_ids <- unique(colData(spe)$sample_id)
+sample_ids <- levels(colData(spe)$sample_id)
 sample_ids
 
 for (s in seq_along(sample_ids)) {
-  spe_sub <- spe[, colData(spe)$sample_id == sample_ids[s] & colData(spe)$tissue == 1]
-  df <- as.data.frame(cbind(colData(spe_sub), spatialData(spe_sub), spatialCoords(spe_sub)))
+  spe_sub <- spe[, colData(spe)$sample_id == sample_ids[s]]
+  df <- as.data.frame(cbind(colData(spe_sub), spatialCoords(spe_sub)))
+  
   for (g in seq_along(human_markers)) {
-    ix_marker <- which(toupper(rowData(spe_sub)$symbol) == toupper(human_markers[g]))
+    ix_marker <- which(toupper(rowData(spe_sub)$gene_name) == toupper(human_markers[g]))
     stopifnot(length(ix_marker) == 1)
     
     # skip gene if total UMI counts below threshold
@@ -93,27 +110,26 @@ for (s in seq_along(sample_ids)) {
     df$marker <- counts(spe_sub)[ix_marker, ]
     
     # plot UMI counts
-    p <- ggplot(df, aes(x = y, y = x, color = marker)) + 
-      geom_point(size = 0.75) + 
+    p <- ggplot(df, aes(x = pxl_col_in_fullres, y = pxl_row_in_fullres, color = marker)) + 
+      geom_point(size = 0.33) + 
       coord_fixed() + 
       scale_y_reverse() + 
-      scale_color_gradient(low = "gray90", high = "blue") + 
+      scale_color_gradient(low = "gray85", high = "red", 
+                           trans = "sqrt", breaks = range(df$marker)) + 
       ggtitle(human_markers[g]) + 
-      labs(color = "UMI counts") + 
+      labs(color = "counts") + 
       theme_bw() + 
       theme(panel.grid = element_blank(), 
             axis.title = element_blank(), 
             axis.text = element_blank(), 
             axis.ticks = element_blank())
     
-    # save plot
-    if (!dir.exists(here("plots", "Mulvey_markers", sample_ids[s]))) {
-      dir.create(here("plots", "Mulvey_markers", sample_ids[s]), recursive = TRUE)
+    if (!dir.exists(here(dir_plots, sample_ids[s]))) {
+      dir.create(here(dir_plots, sample_ids[s]), recursive = TRUE)
     }
-    fn <- here("plots", "Mulvey_markers", sample_ids[s], 
-               paste0(sample_ids[s], "_", human_markers[g]))
-    ggsave(paste0(fn, ".pdf"), plot = p, width = 5.25, height = 4)
-    ggsave(paste0(fn, ".png"), plot = p, width = 5.25, height = 4)
+    fn <- here(dir_plots, sample_ids[s], paste0(sample_ids[s], "_", human_markers[g]))
+    ggsave(paste0(fn, ".pdf"), plot = p, width = 4, height = 3)
+    ggsave(paste0(fn, ".png"), plot = p, width = 4, height = 3)
   }
 }
 
