a3881dca19f94f087c0a60e37e169ed20f553d3f
Lukas Weber <lukas.weber.edu@gmail.com>
2022-05-04 12:03:55 -0400

    pseudobulking

--- code/analyses/06_downstream/LC_downstream_DEtesting.R
+++ code/analyses/06_downstream/LC_downstream_DEtesting.R
@@ -1,7 +1,7 @@
 #################################################################
 # LC project
 # Script for downstream analyses: differential expression testing
-# Lukas Weber, Apr 2022
+# Lukas Weber, May 2022
 #################################################################
 
 # module load conda_R/4.1.x
@@ -13,7 +13,8 @@
 
 library(SpatialExperiment)
 library(here)
-library(BayesSpace)
+library(scater)
+library(scran)
 library(ggplot2)
 
 
@@ -43,4 +44,37 @@ sample_ids
 # ----------------
 
 # pseudobulk spots within LC regions vs. WM regions
+
+# note: using sample_id, not sample_part_id
+
+table(colData(spe)$sample_id, colData(spe)$annot_region)
+
+annot_region_fctr <- factor(as.numeric(colData(spe)$annot_region), labels = c("WM", "LC"))
+table(annot_region_fctr)
+
+ids <- DataFrame(
+  annot_region_pseudo = annot_region_fctr, 
+  sample_id_pseudo = colData(spe)$sample_id
+)
+
+# pseudobulk
+spe_pseudo <- aggregateAcrossCells(spe, ids)
+
+# add levels to colData and column names
+levs <- paste(
+  colData(spe_pseudo)$annot_region_pseudo, 
+  colData(spe_pseudo)$sample_id_pseudo, 
+  sep = "."
+)
+colData(spe_pseudo)$level_id <- levs
+colnames(spe_pseudo) <- levs
+
+# check
+head(colData(spe_pseudo), 3)
+counts(spe_pseudo)[1:3, ]
+
+
+# recalculate logcounts
+# using default library size scale factors
+spe_pseudo <- logNormCounts(spe_pseudo, size.factors = NULL)
 
