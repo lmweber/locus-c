078df214b1a7b97927bfc21dd02c92e978a27f47
Matt N Tran <nnguye44@jhmi.edu>
2022-05-05 21:52:21 -0400

    PW findMarkers with pval.type set to 'some'

--- code/analyses_sn/05_markerDetection.R
+++ code/analyses_sn/05_markerDetection.R
@@ -501,18 +501,271 @@ for(i in othersToCheck){
 }
 
 
+### Looser marker test to characterize some of the 'Neuron.ambig's & 'noDDC' pop ======
+  # Motivation:
+  #   with the standard 'pval.type="all"' argument, it requires that a marker is
+  #   stat. sig. compared to each and every other cluster.  Can loosen this, using
+  #   'pval.type="some"', to try and identify more pairwise markers
+
+# (follow set-up, above)
+
+load(here("processed_data","SCE",
+          "markers-stats_LC-n3_findMarkers_26cellTypes.rda"), verbose=T)
+    # markers.lc.t.pw, markers.lc.t.1vAll, medianNon0.lc.26
+
+## Traditional t-test, pairwise ===
+mod <- with(colData(sce.lc), model.matrix(~ Sample))
+mod <- mod[ , -1, drop=F] # intercept otherwise automatically dropped by `findMarkers()`
+
+# Run pairwise t-tests
+markers.lc.t.pw.some <- findMarkers(sce.lc, groups=sce.lc$cellType.merged,
+                               assay.type="logcounts", design=mod, test="t",
+                               direction="up", pval.type="some", min.prop=0.5, # default for pval.type='some'
+                               full.stats=T)
+
+sapply(markers.lc.t.pw.some, function(x){table(x$FDR<0.05)})
+#
+
+
+
+
+# Add respective 'non0median' column to the stats for each set of markers
+for(i in names(markers.lc.t.pw.some)){
+  markers.lc.t.pw.some[[i]] <- cbind(markers.lc.t.pw.some[[i]],
+                                medianNon0.lc.26[[i]][match(rownames(markers.lc.t.pw.some[[i]]),
+                                                            names(medianNon0.lc.26[[i]]))])
+  colnames(markers.lc.t.pw.some[[i]])[29] <- "non0median"
+}
+
+sapply(markers.lc.t.pw.some, function(x){table(x$FDR<0.05 & x$non0median == TRUE)["TRUE"]})
+    #   ambig.lowNTx.TRUE            Astro.TRUE       Endo.Mural.TRUE          Excit_A.TRUE 
+    #                   6                   216                   214                   336 
+    #        Excit_B.TRUE          Excit_C.TRUE          Excit_D.TRUE          Excit_E.TRUE 
+    #                1925                   908                   796                   284 
+    #        Excit_F.TRUE          Inhib_A.TRUE          Inhib_B.TRUE          Inhib_C.TRUE 
+    #                 385                  1160                   888                   885 
+    #        Inhib_D.TRUE            Micro.TRUE       Neuron.5HT.TRUE Neuron.5HT_noDDC.TRUE 
+    #                  88                   229                   511                   106 
+    # Neuron.ambig_A.TRUE   Neuron.ambig_B.TRUE   Neuron.ambig_C.TRUE   Neuron.ambig_D.TRUE 
+    #                 115                   195                   148                   143 
+    # Neuron.mixed_A.TRUE   Neuron.mixed_B.TRUE        Neuron.NE.TRUE          Oligo_A.TRUE 
+    #                 139                  1945                   629                  1139 
+    #        Oligo_B.TRUE              OPC.TRUE 
+    #                 114                   334 
+
+
+    ## Compared to, with 'pval.type="all"'
+            sapply(markers.lc.t.pw, function(x){table(x$FDR<0.05 & x$non0median == TRUE)["TRUE"]})
+            #    ambig.lowNTx.NA          Astro.TRUE     Endo.Mural.TRUE        Excit_A.TRUE 
+            #                 NA                  79                  62                   2 
+            #       Excit_B.TRUE        Excit_C.TRUE        Excit_D.TRUE        Excit_E.TRUE 
+            #                 15                  18                  87                  38 
+            #       Excit_F.TRUE        Inhib_A.TRUE        Inhib_B.TRUE        Inhib_C.TRUE 
+            #                 47                   1                   6                 142 
+            #       Inhib_D.TRUE          Micro.TRUE     Neuron.5HT.TRUE Neuron.5HT_noDDC.NA 
+            #                  3                 123                  65                  NA 
+            #Neuron.ambig_A.TRUE Neuron.ambig_B.TRUE   Neuron.ambig_C.NA Neuron.ambig_D.TRUE 
+            #                  1                   5                  NA                   1 
+            #  Neuron.mixed_A.NA Neuron.mixed_B.TRUE      Neuron.NE.TRUE        Oligo_A.TRUE 
+            #                 NA                1017                  95                 484 
+            #         Oligo_B.NA            OPC.TRUE 
+            #                 NA                  66 
+
+            
+            
+save(markers.lc.t.pw.some, medianNon0.lc.26,
+     file=here("processed_data","SCE",
+          "markers-stats_LC-n3_findMarkers-PW-some_26cellTypes.rda"))
+
+printSomeMarkers <- c("Excit_A", "Inhib_A", "Inhib_D", "Neuron.5HT_noDDC",
+                paste0("Neuron.ambig_",c("A","B","C","D")), "Oligo_B")
+            
+# Print these to pngs
+markerList.t.pw <- lapply(markers.lc.t.pw.some, function(x){
+  rownames(x)[x$FDR < 0.05 & x$non0median == TRUE]
+}
+)
+# Change to gene symbols
+markerList.t.pw <- lapply(markerList.t.pw, function(x){
+  rowData(sce.lc)$gene_name[match(x, rowData(sce.lc)$gene_id)]
+})
+head(markerList.t.pw[["Inhib_D"]], n=10)
+
+    ## These actually look like the '1vAllOthers' markers...
+    markerList.t.1vAll <- lapply(markers.lc.t.1vAll, function(x){
+      rownames(x[[2]])[ x[[2]]$log.FDR < log(0.05) & x[[2]]$non0median==TRUE ]
+    }
+    )
+    
+    # Change to gene symbols
+    markerList.t.1vAll <- lapply(markerList.t.1vAll, function(x){
+      rowData(sce.lc)$gene_name[match(x, rowData(sce.lc)$gene_id)]
+    })
+    
+    sapply(printSomeMarkers, function(x){
+      length(intersect(markerList.t.pw[[x]],
+                       markerList.t.1vAll[[x]]))
+    })
+        #        Excit_A          Inhib_A          Inhib_D Neuron.5HT_noDDC   Neuron.ambig_A 
+        #            335             1159               87              102              115 
+        # Neuron.ambig_B   Neuron.ambig_C   Neuron.ambig_D          Oligo_B 
+        #            194              147              143              113 
+
+            # & looking above, this is probably ~98% of them overlapping...
+
+# Just print 20 of these 'some' markers
+for(i in printSomeMarkers){
+  png(here("plots","snRNA-seq","markers",
+           paste0("LC_t_pairwise-SOME_topMarkers-", i, "_vlnPlots.png")), height=950, width=1200)
+  print(
+    plotExpressionCustom(sce = sce.hold,
+                         exprs_values = "logcounts",
+                         features = head(markerList.t.pw[[i]],n=20), 
+                         features_name = i,
+                         anno_name = "cellType.merged",
+                         ncol=5, point_alpha=0.4,
+                         scales="free_y", swap_rownames="gene_name") +
+      scale_color_manual(values = c(tableau20, tableau10medium)) +  
+      ggtitle(label=paste0("LC ", i, " top markers: single-nucleus-level p.w.-SOME t-tests (FDR<0.05)")) +
+      theme(plot.title = element_text(size = 20))
+  )
+  dev.off()
+}
+
+
+
 
 
 
 ## Reproducibility information ====
 print('Reproducibility information:')
 Sys.time()
-# 
+    # [1] "2022-05-05 21:43:37 EDT"
 proc.time()
-#     user    system   elapsed 
-#
+    #     user    system   elapsed 
+    # 2072.283    87.553 16963.639 
 options(width = 120)
 session_info()
-
+    #─ Session info ────────────────────────────────────────────────────────────────────────────────
+    # setting  value
+    # version  R version 4.1.2 Patched (2021-11-04 r81138)
+    # os       CentOS Linux 7 (Core)
+    # system   x86_64, linux-gnu
+    # ui       X11
+    # language (EN)
+    # collate  en_US.UTF-8
+    # ctype    en_US.UTF-8
+    # tz       US/Eastern
+    # date     2022-05-05
+    # pandoc   2.13 @ /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.1.x/bin/pandoc
+    # 
+    # ─ Packages ────────────────────────────────────────────────────────────────────────────────────
+    # package              * version  date (UTC) lib source
+    # assertthat             0.2.1    2019-03-21 [2] CRAN (R 4.1.0)
+    # batchelor            * 1.10.0   2021-10-26 [1] Bioconductor
+    # beachmat               2.10.0   2021-10-26 [2] Bioconductor
+    # beeswarm               0.4.0    2021-06-01 [2] CRAN (R 4.1.2)
+    # Biobase              * 2.54.0   2021-10-26 [2] Bioconductor
+    # BiocGenerics         * 0.40.0   2021-10-26 [2] Bioconductor
+    # BiocNeighbors          1.12.0   2021-10-26 [2] Bioconductor
+    # BiocParallel           1.28.3   2021-12-09 [2] Bioconductor
+    # BiocSingular           1.10.0   2021-10-26 [2] Bioconductor
+    # bitops                 1.0-7    2021-04-24 [2] CRAN (R 4.1.0)
+    # bluster              * 1.4.0    2021-10-26 [2] Bioconductor
+    # cli                    3.3.0    2022-04-25 [2] CRAN (R 4.1.2)
+    # cluster                2.1.3    2022-03-28 [3] CRAN (R 4.1.2)
+    # colorspace             2.0-3    2022-02-21 [2] CRAN (R 4.1.2)
+    # cowplot                1.1.1    2020-12-30 [2] CRAN (R 4.1.2)
+    # crayon                 1.5.1    2022-03-26 [2] CRAN (R 4.1.2)
+    # DBI                    1.1.2    2021-12-20 [2] CRAN (R 4.1.2)
+    # DelayedArray           0.20.0   2021-10-26 [2] Bioconductor
+    # DelayedMatrixStats     1.16.0   2021-10-26 [2] Bioconductor
+    # digest                 0.6.29   2021-12-01 [2] CRAN (R 4.1.2)
+    # dplyr                  1.0.9    2022-04-28 [2] CRAN (R 4.1.2)
+    # dqrng                  0.3.0    2021-05-01 [2] CRAN (R 4.1.2)
+    # DropletUtils         * 1.14.2   2022-01-09 [2] Bioconductor
+    # dynamicTreeCut       * 1.63-1   2016-03-11 [1] CRAN (R 4.1.2)
+    # edgeR                  3.36.0   2021-10-26 [2] Bioconductor
+    # ellipsis               0.3.2    2021-04-29 [2] CRAN (R 4.1.0)
+    # fansi                  1.0.3    2022-03-24 [2] CRAN (R 4.1.2)
+    # farver                 2.1.0    2021-02-28 [2] CRAN (R 4.1.0)
+    # fs                     1.5.2    2021-12-08 [2] CRAN (R 4.1.2)
+    # gargle                 1.2.0    2021-07-02 [2] CRAN (R 4.1.0)
+    # generics               0.1.2    2022-01-31 [2] CRAN (R 4.1.2)
+    # GenomeInfoDb         * 1.30.1   2022-01-30 [2] Bioconductor
+    # GenomeInfoDbData       1.2.7    2021-11-01 [2] Bioconductor
+    # GenomicRanges        * 1.46.1   2021-11-18 [2] Bioconductor
+    # ggbeeswarm             0.6.0    2017-08-07 [2] CRAN (R 4.1.2)
+    # ggplot2              * 3.3.6    2022-05-03 [2] CRAN (R 4.1.2)
+    # ggrepel                0.9.1    2021-01-15 [2] CRAN (R 4.1.0)
+    # glue                   1.6.2    2022-02-24 [2] CRAN (R 4.1.2)
+    # googledrive            2.0.0    2021-07-08 [2] CRAN (R 4.1.0)
+    # gridExtra            * 2.3      2017-09-09 [2] CRAN (R 4.1.0)
+    # gtable                 0.3.0    2019-03-25 [2] CRAN (R 4.1.0)
+    # HDF5Array              1.22.1   2021-11-14 [2] Bioconductor
+    # here                 * 1.0.1    2020-12-13 [2] CRAN (R 4.1.2)
+    # igraph                 1.3.1    2022-04-20 [2] CRAN (R 4.1.2)
+    # IRanges              * 2.28.0   2021-10-26 [2] Bioconductor
+    # irlba                  2.3.5    2021-12-06 [2] CRAN (R 4.1.2)
+    # jaffelab             * 0.99.31  2021-12-13 [1] Github (LieberInstitute/jaffelab@2cbd55a)
+    # labeling               0.4.2    2020-10-20 [2] CRAN (R 4.1.0)
+    # lattice                0.20-45  2021-09-22 [3] CRAN (R 4.1.2)
+    # lifecycle              1.0.1    2021-09-24 [2] CRAN (R 4.1.2)
+    # limma                  3.50.3   2022-04-07 [2] Bioconductor
+    # locfit                 1.5-9.5  2022-03-03 [2] CRAN (R 4.1.2)
+    # magrittr               2.0.3    2022-03-30 [2] CRAN (R 4.1.2)
+    # Matrix                 1.4-1    2022-03-23 [3] CRAN (R 4.1.2)
+    # MatrixGenerics       * 1.6.0    2021-10-26 [2] Bioconductor
+    # matrixStats          * 0.62.0   2022-04-19 [2] CRAN (R 4.1.2)
+    # metapod                1.2.0    2021-10-26 [2] Bioconductor
+    # munsell                0.5.0    2018-06-12 [2] CRAN (R 4.1.0)
+    # pheatmap             * 1.0.12   2019-01-04 [2] CRAN (R 4.1.0)
+    # pillar                 1.7.0    2022-02-01 [2] CRAN (R 4.1.2)
+    # pkgconfig              2.0.3    2019-09-22 [2] CRAN (R 4.1.0)
+    # purrr                  0.3.4    2020-04-17 [2] CRAN (R 4.1.0)
+    # R.methodsS3            1.8.1    2020-08-26 [2] CRAN (R 4.1.0)
+    # R.oo                   1.24.0   2020-08-26 [2] CRAN (R 4.1.0)
+    # R.utils                2.11.0   2021-09-26 [2] CRAN (R 4.1.2)
+    # R6                     2.5.1    2021-08-19 [2] CRAN (R 4.1.2)
+    # rafalib              * 1.0.0    2015-08-09 [1] CRAN (R 4.1.2)
+    # RColorBrewer           1.1-3    2022-04-03 [2] CRAN (R 4.1.2)
+    # Rcpp                   1.0.8.3  2022-03-17 [2] CRAN (R 4.1.2)
+    # RCurl                  1.98-1.6 2022-02-08 [2] CRAN (R 4.1.2)
+    # ResidualMatrix         1.4.0    2021-10-26 [1] Bioconductor
+    # rhdf5                  2.38.1   2022-03-10 [2] Bioconductor
+    # rhdf5filters           1.6.0    2021-10-26 [2] Bioconductor
+    # Rhdf5lib               1.16.0   2021-10-26 [2] Bioconductor
+    # rlang                  1.0.2    2022-03-04 [2] CRAN (R 4.1.2)
+    # rprojroot              2.0.3    2022-04-02 [2] CRAN (R 4.1.2)
+    # rsvd                   1.0.5    2021-04-16 [2] CRAN (R 4.1.2)
+    # S4Vectors            * 0.32.4   2022-03-24 [2] Bioconductor
+    # ScaledMatrix           1.2.0    2021-10-26 [2] Bioconductor
+    # scales                 1.2.0    2022-04-13 [2] CRAN (R 4.1.2)
+    # scater               * 1.22.0   2021-10-26 [2] Bioconductor
+    # scran                * 1.22.1   2021-11-14 [2] Bioconductor
+    # scry                 * 1.6.0    2021-10-26 [2] Bioconductor
+    # scuttle              * 1.4.0    2021-10-26 [2] Bioconductor
+    # segmented              1.3-4    2021-04-22 [1] CRAN (R 4.1.2)
+    # sessioninfo          * 1.2.2    2021-12-06 [2] CRAN (R 4.1.2)
+    # SingleCellExperiment * 1.16.0   2021-10-26 [2] Bioconductor
+    # sparseMatrixStats      1.6.0    2021-10-26 [2] Bioconductor
+    # statmod                1.4.36   2021-05-10 [2] CRAN (R 4.1.0)
+    # SummarizedExperiment * 1.24.0   2021-10-26 [2] Bioconductor
+    # tibble                 3.1.7    2022-05-03 [2] CRAN (R 4.1.2)
+    # tidyselect             1.1.2    2022-02-21 [2] CRAN (R 4.1.2)
+    # utf8                   1.2.2    2021-07-24 [2] CRAN (R 4.1.0)
+    # vctrs                  0.4.1    2022-04-13 [2] CRAN (R 4.1.2)
+    # vipor                  0.4.5    2017-03-22 [2] CRAN (R 4.1.2)
+    # viridis                0.6.2    2021-10-13 [2] CRAN (R 4.1.2)
+    # viridisLite            0.4.0    2021-04-13 [2] CRAN (R 4.1.0)
+    # withr                  2.5.0    2022-03-03 [2] CRAN (R 4.1.2)
+    # XVector                0.34.0   2021-10-26 [2] Bioconductor
+    # zlibbioc               1.40.0   2021-10-26 [2] Bioconductor
+    # 
+    # [1] /users/ntranngu/R/4.1.x
+    # [2] /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.1.x/R/4.1.x/lib64/R/site-library
+    # [3] /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.1.x/R/4.1.x/lib64/R/library
+    # 
+    # ───────────────────────────────────────────────────────────────────────────────────────────────
 
 
