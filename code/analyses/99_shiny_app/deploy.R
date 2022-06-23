d6e2e10ecf323768e412168ffda8c2b1a39342e7
Lukas Weber <lukas.weber.edu@gmail.com>
2022-06-23 15:14:33 -0400

    redeploy app with clean SPE object

--- code/analyses/99_shiny_app/deploy.R
+++ code/analyses/99_shiny_app/deploy.R
@@ -8,7 +8,7 @@ library(rsconnect)
 library(here)
 
 
-## Or you can go to your shinyapps.io account and copy this
+## Or go to your shinyapps.io account and copy this
 ## Here we do this to keep our information hidden
 load(here("code", "analyses", "99_shiny_app", "deploy_info.RData"), verbose = TRUE)
 rsconnect::setAccountInfo(
@@ -17,10 +17,10 @@ rsconnect::setAccountInfo(
   secret = deploy_info$secret
 )
 
-## You need this to enable shinyapps to install Bioconductor packages
+## Required to enable shinyapps to install Bioconductor packages
 options(repos = BiocManager::repositories())
 
-## Deploy the app, that is, upload it to shinyapps.io
+## Deploy the app by uploading to shinyapps.io
 rsconnect::deployApp(
   appDir = here("code", "analyses", "99_shiny_app"), 
   appFiles = c(
