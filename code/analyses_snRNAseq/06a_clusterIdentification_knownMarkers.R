c39871ed9bdd7d61e7e9bb9b3bbfd7d900262c82
Lukas Weber <lukas.weber.edu@gmail.com>
2022-09-08 14:49:30 -0400

    clean up

--- code/analyses_snRNAseq/06a_clusterIdentification_knownMarkers.R
+++ code/analyses_snRNAseq/06a_clusterIdentification_knownMarkers.R
@@ -453,22 +453,6 @@ hm
 dev.off()
 
 
-
-# palettes from scater
-tableau20 = c("#1F77B4", "#AEC7E8", "#FF7F0E", "#FFBB78", "#2CA02C", 
-              "#98DF8A", "#D62728", "#FF9896", "#9467BD", "#C5B0D5", 
-              "#8C564B", "#C49C94", "#E377C2", "#F7B6D2", "#7F7F7F", 
-              "#C7C7C7", "#BCBD22", "#DBDB8D", "#17BECF", "#9EDAE5")
-tableau10medium = c("#729ECE", "#FF9E4A", "#67BF5C", "#ED665D", 
-                    "#AD8BC9", "#A8786E", "#ED97CA", "#A2A2A2", 
-                    "#CDCC5D", "#6DCCDA")
-colors <- unique(c(tableau20, tableau10medium))
-
-
-
-
-
-
 # ----------------------------------------------
 # Marker expression heatmap: inhibitory subtypes
 # ----------------------------------------------
