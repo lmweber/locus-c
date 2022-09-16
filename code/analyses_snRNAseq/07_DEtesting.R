25097d1a444cc3c0ca0d6b419ca8c31e637d0d31
Lukas Weber <lukas.weber.edu@gmail.com>
2022-09-15 22:09:53 -0400

    add 5-HT DE testing plot

--- code/analyses_snRNAseq/07_DEtesting.R
+++ code/analyses_snRNAseq/07_DEtesting.R
@@ -121,9 +121,9 @@ table(marker_info[["6"]]$FDR < 1e-100)
 # #View(as.data.frame(ordered))
 
 
-# -------------
-# Volcano plots
-# -------------
+# -------------------------
+# Volcano plots: NE neurons
+# -------------------------
 
 # NE neurons
 
@@ -176,6 +176,58 @@ ggplot(df, aes(x = log2FC, y = -log10(FDR), color = highlysig, label = gene)) +
         panel.grid.minor = element_blank())
 
 fn <- file.path(dir_plots, "DEtesting_NEneuronsVsAllOtherNeuronalClusters")
+ggsave(paste0(fn, ".pdf"), width = 4.5, height = 4)
+ggsave(paste0(fn, ".png"), width = 4.5, height = 4)
+
+
+# ---------------------------
+# Volcano plots: 5-HT neurons
+# ---------------------------
+
+# 5-HT neurons
+
+fdr <- marker_info[["16"]]$FDR
+logfc <- marker_info[["16"]]$summary.logFC
+
+names(fdr) <- names(logfc) <- marker_info[["16"]]$gene_name
+
+
+# highly significant thresholds
+highlysig <- (fdr < 1e-20) & (logfc > log2(4))
+
+# number of significant genes
+table(highlysig)
+
+
+df <- data.frame(
+  gene = names(fdr), 
+  FDR = fdr, 
+  log2FC = logfc, 
+  highlysig = highlysig
+)
+
+pal <- c("black", "red")
+
+
+# volcano plot with labels
+set.seed(123)
+ggplot(df, aes(x = log2FC, y = -log10(FDR), color = highlysig, label = gene)) + 
+  geom_point(size = 0.1) + 
+  geom_point(data = df[df$highlysig, ], size = 0.5) + 
+  geom_text_repel(data = df[df$highlysig, ], 
+                  size = 1.5, nudge_y = 0.1, 
+                  force = 0.1, force_pull = 0.1, min.segment.length = 0.1, 
+                  max.overlaps = 20) + 
+  scale_color_manual(values = pal, guide = "none") + 
+  geom_hline(yintercept = -log10(1e-20), lty = "dashed", color = "royalblue") + 
+  #geom_vline(xintercept = -thresh_logfc, lty = "dashed", color = "royalblue") + 
+  geom_vline(xintercept = log2(4), lty = "dashed", color = "royalblue") + 
+  ggtitle("NE neuron cluster vs. all neuronal clusters") + 
+  theme_bw() + 
+  theme(plot.title = element_text(face = "bold"), 
+        panel.grid.minor = element_blank())
+
+fn <- file.path(dir_plots, "DEtesting_5HTneuronsVsAllOtherNeuronalClusters")
 ggsave(paste0(fn, ".pdf"), width = 4.5, height = 4)
 ggsave(paste0(fn, ".png"), width = 4.5, height = 4)
 
