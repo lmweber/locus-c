5800c8dd04c1874babd60424543689fd7cbfd088
Lukas Weber <lukas.weber.edu@gmail.com>
2022-10-05 15:12:08 -0400

    update columns and column order in EHub SPE object

--- code/analyses/03c_LC_ExperimentHub.R
+++ code/analyses/03c_LC_ExperimentHub.R
@@ -32,17 +32,21 @@ table(colData(spe)$sample_id)
 # clean dataset for public release
 # --------------------------------
 
-# remove unused columns from colData
+# remove unused columns from colData and re-order
 
 # not using 'cell_count' (number of cells per spot)
+# not using 'total' (duplicate column)
 # not using 'high_subsets_mito_percent' (QC filtering on mitochondrial percentage per spot)
 
-ix_cols_remove <- c(8, 21)
-
+ix_cols_remove <- c(8, 18, 21)
 colnames(colData(spe))[ix_cols_remove]
 
 # remove columns
 colData(spe) <- colData(spe)[, -ix_cols_remove]
+
+# re-order remaining columns
+ix_cols_order <- c(4:7, 10:11, 1:3, 8:9, 12:20)
+colData(spe) <- colData(spe)[, ix_cols_order]
 
 # check
 head(colData(spe), 2)
